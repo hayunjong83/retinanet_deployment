60984997bef639ef743d8b12b509ce5d09b8d3ba
hayunjong83 <hayunjong83@gmail.com>
2021-04-14 15:46:57 +0900

    investigate Image upload sample implementation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import DropZone from "./dropzone/DropZone";
+import DropZone from "./component/dropzone/DropZone";
 
 function App() {
   return (
     <div>
-      <p className="title">Fracture Detection using RetinaNet</p>
+      <p className="title">Object Detection using RetinaNet</p>
       <div className="content">
         <DropZone />
       </div>
